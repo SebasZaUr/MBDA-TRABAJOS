1bc8681063a6ebc4a779ac9c41bd459ac2f38a51
XxELIngexX <114599685+XxELIngexX@users.noreply.github.com>
2022-11-24 13:01:24 -0500

    Update AutoEstudio6.sql

--- auto6/AutoEstudio6.sql
+++ auto6/AutoEstudio6.sql
@@ -1,23 +1,36 @@
 CREATE TABLE ProductCategory(
     ProductCategoryID NUMBER(4) NOT NULL,
     ParentProductCategoryID NUMBER(4),
-    Name_ VARCHAR(50) NOT NULL,
-    another_info SYS.XMLType);
+    nombre VARCHAR(50) NOT NULL,
+    prueba SYS.XMLType);
 --- insertacion :v
-INSERT INTO ProductCategory (ProductCategoryID,ParentProductCategory,Name_,another_info) 
-  VALUES (123,2,'La Categoria mas Chimba',XMLType.createXML(
-  '<Infocategory>
-    <deporte>
-        <Asociado deporteAsociado = "ciclismo"></Asociado>
-    </deporte>
+INSERT INTO ProductCategory (ProductCategoryID, ParentProductCategoryID, nombre, prueba) 
+VALUES (123,2,'La Categoria mas Chimba',SYS.XMLType.createXML(
+'<!DOCTYPE prueba[
+    <!ELEMENT prueba (deportes,peso,colores)>
+    <!ELEMENT deportes (Asociado)>
+    <!ELEMENT peso (pesos)>
+    <!ELEMENT colores (opciones)>
+    <!ELEMENT Asociado (#PCDATA)>
+    <!ELEMENT pesos (#PCDATA)>
+    <!ELEMENT opciones (#PCDATA)>
+    <!ATTLIST pesos minimo CDATA #IMPLIED>
+    <!ATTLIST pesos maximo CDATA #REQUIRED>
+    <!ATTLIST opciones color1 CDATA #IMPLIED>
+    <!ATTLIST opciones color2 CDATA #REQUIRED>
+    <!ATTLIST Asociado deporteAsociado CDATA #REQUIRED>
+]>
+
+<prueba>
+     <deportes>
+        <Asociado deporteAsociado = "ciclismo"> </Asociado>
+    </deportes>
     
     <peso>
-        <pesos minimo = "0", maximo = "100" ></pesos>
+        <pesos minimo = "0" maximo = "100"> </pesos>
     </peso>
-    <colores
-        <opciones color1= "negro", color2="rojo" >
+    <colores>
+        <opciones color1 = "negro" color2 = "rojo"></opciones>
     </colores>
-  </Infocategory>'));
-  
-DELETE FROM ProductCategory;
-
+</prueba>'));
+DROP  TABLE ProductCategory CASCADE CONSTRAINTS;
